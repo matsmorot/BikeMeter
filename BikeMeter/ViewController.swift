72ad68ff3ede737d0b26d392af0da2b8513ee5da
matsmorot <matsmorot@gmail.com>
2017-03-22 13:22:24 +0100

    Initial Commit

---
+++ BikeMeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeMeter
+//
+//  Created by Mattias Almén on 2017-03-22.
+//  Copyright © 2017 pixlig.se. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
